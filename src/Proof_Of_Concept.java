9c0ef8effe4ca205340b96fce5b1600d22b1904b
vardaofthevalier <abigail.n.hahn@gmail.com>
2014-03-30 16:31:12 -0400

    Continued work on the assembler, created a test program file and modified the proof of concept program.

--- src/Proof_Of_Concept.java
+++ src/Proof_Of_Concept.java
@@ -1,10 +1,26 @@
+import java.io.IOException;
+
 
 public class Proof_Of_Concept {
 	
-	// First, assemble a SAG Assembly Language program into machine language that can be understood by the SAG CPU
+	public static void main(String [] args){
 	
-	// Initialize the SAG CPU
+		// First, assemble a SAG Assembly Language program into machine language that can be understood by the SAG CPU
 	
-	// Run the assembled program on the SAG CPU, watch the magic happen! :)
+		SAG_Language_Assembler assembler = SAG_Language_Assembler.getInstance();
+		
+		String inputFile = new String("test_program.sag");
+		String outputFile = new String("test_program.out");
+		try {
+			assembler.AssembleProgram(inputFile, outputFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		// Initialize the SAG CPU
+	
+		// Run the assembled program on the SAG CPU, watch the magic happen! :)
+	}
 
 }
--- src/SAG_Language_Assembler.java
+++ src/SAG_Language_Assembler.java
@@ -1,21 +1,70 @@
+/* Grammar Rules 
+		 * 
+		 * Program Parse:
+		 * 
+		 * <program> -> PROGRAM <identifier>
+		 * 				{ <data> }
+		 * 				{ <routine> }
+		 * 				END <identifier>
+		 * 
+		 * <data> -> { <identifier> <value> | <pseudo-instruction> }
+		 * 
+		 * <routine> -> BEGIN <identifier>
+		 * 				{ <instruction> }
+		 * 				END <identifier>
+		 * 
+		 * 
+		 * Instruction Parse:
+		 * 
+		 * <instruction> -> <math-op> <reg> <reg> <reg> | <imm-op> <reg> <imm-value> | <jump-op> <address>
+		 * 
+		 * <pseudo-instruction> -> (not sure yet)
+		 * 
+		 * <math-op> -> NOOP | ADD | SUB | AND | OR | NOT | SLL | SLR | SLT | BEQ | BNEQ
+		 * 
+		 * <reg> -> %<decimal-number>
+		 * 
+		 * <imm-op> -> ADDI | SLTI | LOAD | STORE
+		 * 
+		 * <imm-value> -> <binary-number>  
+		 * 
+		 * <jump-op> -> JUMP
+		 * 
+		 * <address> -> <binary-number>   
+		 * 
+		 * 
+		 * Tokenizer:
+		 * 
+		 * <identifier> -> { <capital-letter> } 
+		 * 
+		 * <value> -> <binary-number>
+		 * 
+		 * <binary-number> -> { 0 | 1 }
+		 * 
+		 * <decimal-number> -> { 0 | 1 | 2 | 3 | ... | 15 }
+		 * 
+		 * <capital-letter> -> A | B | C | ... | Z
+		 * 
+		 * */
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class SAG_Language_Assembler {
 	
-	// Member variables
+	// Constructor
 	
 	private static SAG_Language_Assembler instance = null; 
-
-	// Constructor
 	
 	private SAG_Language_Assembler()
 	{
-		// Empty constructor 
+		// Empty constructor (Singleton)
 	}
-	
-	// Methods
 	
 	public static synchronized SAG_Language_Assembler getInstance()
 	{
@@ -27,7 +76,9 @@ public class SAG_Language_Assembler {
 		return instance;
 	}
 	
-	public static class ParseTree
+	// Additional Classes/Enums
+	
+	public class ParseTree
 	{
 		String root;
 		ArrayList<ParseTree> children;
@@ -40,21 +91,55 @@ public class SAG_Language_Assembler {
 		
 		public void Insert(String token)
 		{
-			
+			ParseTree newChild = new ParseTree(token);
+			this.children.add(newChild);
 		}
 		
 	}
 	
-	public void AssembleProgram(File program, File destination)
+	public enum ArithmeticOperations
+	{
+		NOOP, ADD, SUB, AND, OR, NOT, SLL, SLR, SLT, BEQ, BNEQ
+	}
+	
+	public enum ImmediateOperations
+	{
+		ADDI, SLTI, LOAD, STORE
+	}
+	
+	public enum JumpOperations
+	{
+		JUMP
+	}
+	
+	public enum PseudoOperations
+	{
+		// List of pseudo operations
+	}
+	
+	// Methods
+	
+	public void AssembleProgram(String inputFile, String outputFile) throws IOException
 	{
 		// Read in the file ("program") as a string
-		String prog = new String("Placeholder");
+		BufferedReader fileReader = new BufferedReader(new FileReader(inputFile));
+		String nextLine = null;
+		StringBuilder fileContents = new StringBuilder();
+		String separator = System.getProperty("line.separator");
+		
+		while((nextLine = fileReader.readLine()) != null){
+	        fileContents.append(nextLine);
+	        fileContents.append(separator);
+	    }
+		
+		fileReader.close();
 		
 		// Break into tokens
-		String[] tokens = Tokenize(prog);
+		
+		ArrayList<String> tokens = Tokenize(fileContents.toString());
 		
 		// Parse
-		ParseTree ptree = Parse(tokens);
+		ParseTree ptree = ProgramParse(tokens);
 		
 		// Generate machine code
 		String machineCode = GenerateCode(ptree);
@@ -62,27 +147,69 @@ public class SAG_Language_Assembler {
 		// Write the machine code to a file ("destination")
 		
 	}
-	public String[] Tokenize(String program)
-	{
-		// Break the SAG Assembly Language Program into tokens
-		String[] tokens = program.split("\\s");
-		// Return the string of tokens
-		return tokens;
+
+	public static ArrayList<String> Tokenize(String prog)
+	{		
+		ArrayList<String> tokens = new ArrayList<String>(Arrays.asList(prog.split("\\s")));
 		
+		return tokens;
 	}
 	
-	public ParseTree Parse(String[] tokens)
+	public ParseTree ProgramParse(ArrayList<String> tokens)
 	{
 		// Create a parse tree out of the string of tokens using the grammar rules for the language
-		ParseTree ptree = new ParseTree("Placeholder");
+		ParseTree program = new ParseTree("");
+		
+		if (!tokens.isEmpty()){
+			String nextToken = tokens.remove(0);
+			
+			if (nextToken == "PROGRAM")
+			{
+				program.root = nextToken;
+				
+				if (!tokens.isEmpty()){
+				
+					nextToken = tokens.remove(0);
+					
+					if (isValidIdentifier(nextToken))
+					{
+						program.Insert(nextToken);
+					}
+					
+					else
+					{
+						System.out.println("Error: The program identifier is not valid.");
+						
+					}
+					
+					while (!tokens.isEmpty() && nextToken != "END"){
+						// Recursively descend
+					}
+				}
+			}
+			else{
+				// Not a valid program, report error
+			}
+		
+		}
+		else{
+			// Nothing to assemble, report error
+		}
 		
 		// Lots of stuff here
 		
 		// Return the parse tree
-		return ptree;
+		return program;
 	}
 	
+	public ParseTree InstructionParse(ArrayList tokens)
+	{
+		ParseTree itree = new ParseTree("Placeholder");
+		
+		return itree;
+	}
 	
+
 	public String GenerateCode(ParseTree ptree)
 	{
 		// Use the parse tree to generate the machine code
@@ -90,6 +217,12 @@ public class SAG_Language_Assembler {
 		
 		// Write the machine code to a file
 		return machineCode;
+	}
+	
+	public boolean isValidIdentifier(String token){
+		boolean isValidIdentifier = false;
+		
+		return isValidIdentifier;
 	}
 
 }
