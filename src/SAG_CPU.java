af0de7ab660b9bf5cb6d8c7affbe75f36d0ca5fd
Joseph Wortman <wortman.30@buckeyemail.osu.edu>
2014-04-09 14:44:36 -0400

    Changed All registers to GP registers

--- src/SAG_CPU.java
+++ src/SAG_CPU.java
@@ -12,7 +12,7 @@ public class SAG_CPU {
 	private Register IR;
 	private Register Zero;
 	private Register[] GP_Registers;
-	private Register[] Device_Registers;
+	//private Register[] Device_Registers;
 	private Register Stack_Pointer;
 	private Register Frame_Pointer;
 	private Register Return_Address;
@@ -23,8 +23,8 @@ public class SAG_CPU {
 	public SAG_CPU(){
 		this.PC = new Program_Counter();
 		this.IR = new Register();
-		this.GP_Registers = new Register[7];
-		this.Device_Registers = new Register[4];
+		this.GP_Registers = new Register[11];
+		//this.Device_Registers = new Register[4];
 		this.Stack_Pointer = new Register();
 		this.Frame_Pointer = new Register();
 		this.Return_Address = new Register();
